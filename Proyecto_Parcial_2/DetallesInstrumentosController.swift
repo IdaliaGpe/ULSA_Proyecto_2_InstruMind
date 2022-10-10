387330621bc8ba0df21fbee41e4f63367130788b
Alumno <cesarrgomez@hotmail.com>
2022-10-10 08:20:48 -0700

    Contrains Listos

--- Proyecto_Parcial_2/DetallesInstrumentosController.swift
+++ Proyecto_Parcial_2/DetallesInstrumentosController.swift
@@ -67,6 +67,6 @@ class DetallesInstrumentosController: UIViewController, UITableViewDelegate, UIT
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 81
+        return 69
     }
 }
